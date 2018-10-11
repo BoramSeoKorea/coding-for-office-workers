bc1437eb6b5b0b422408b84d982bb7a2fd846c6f
BORAM SEO <BORAM_SEO@symantec.com>
2018-10-11 13:48:53 +0800

    Add 02-data-type-1.py

---
+++ 02-data-type-1.py
@@ -0,0 +1,14 @@
+#정수int - integer
+print(1+1)
+#소수float
+print(1.5+2.1)
+#문자str - string
+print("hello")
+
+#print("hello"+"1")
+#print(4//2)
+#print(int(4/2))
+#print(float(2))
+print("1")
+print(int("1"))
+print(int("1.5"))
