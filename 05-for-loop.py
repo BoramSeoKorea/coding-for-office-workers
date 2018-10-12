924e763bf0b322d9561e2be2a54d2ff9b14b0054
BORAM SEO <BORAM_SEO@symantec.com>
2018-10-12 09:24:28 +0800

    Add 05-for-loop.py

---
+++ 05-for-loop.py
@@ -0,0 +1,21 @@
+#for, range
+#while
+
+#for
+#cf, [1,2,3][0:2]레인지 및 슬라이스는 마지막 숫자 포함하지 않는다
+for num in range(1,10):
+    print(num)
+
+num_list=[1,2,3,4,5,6,7,8,9]
+for num in num_list:
+    print(num)
+
+#while
+#while True: #무한루프
+#    print(1)
+
+a=1
+while a<10:
+    print(a)
+    a=a+1
+    
